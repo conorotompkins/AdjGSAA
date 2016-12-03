3efc235fe15716f665615c8d6a567c2c6d44815f
conorotompkins <conorotompkins@gmail.com>
2016-12-03 18:23:22 -0500

    working on charts

--- graphs/MAF coach/maf coaching graphs.R
+++ graphs/MAF coach/maf coaching graphs.R
@@ -9,15 +9,20 @@ library(tidyr)
 library(cowplot)
 library(forcats)
 library(RCurl)
+library(viridis)
 
-maf <- df_combined <- read.csv(text=getURL("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/combined/adjgsaa.combined.csv")) %>%
+source("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/graphs/theme_nhh.R")
+
+maf <- read.csv(text=getURL("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/combined/adjgsaa.combined.csv")) %>%
   filter(name == "Marc-Andre.Fleury", toi > 2) %>%
   select(game_type, name, season, date, game_number, adjgsaa60) %>%
-  mutate(coach = ifelse(date < "2013-06-23", "Giles Meloche", "Mike Bales"), 
-         season = as.character(season), 
-         team = as.character(team), 
+  group_by(name, season) %>%
+  mutate(season_game_number = dense_rank(date)) %>%
+  ungroup() %>%
+  mutate(season = as.character(season), 
          name = as.character(name), 
-         date = ymd(date))
+         date = ymd(date),
+         coach = ifelse(date < "2013-06-23", "Giles Meloche", "Mike Bales"))
 
 ggplot(maf, aes(game_number, adjgsaa60)) +
   geom_hline(yintercept = 0) +
@@ -29,17 +34,20 @@ ggplot(maf, aes(game_number, adjgsaa60)) +
   labs(y = "Adjusted GSAA per 60", x = "Game Number", title = "Marc-Andre Fleury Goalie Coach Comparison") +
   theme_nhh()
 
-ggplot(maf, aes(Game.Number, adjgsaa60, color = coach, fill = coach)) +
+ggplot(maf, aes(season_game_number, adjgsaa60)) +
   geom_hline(yintercept = 0) +
-  geom_vline(xintercept = 530) +
-  geom_point(size = 2, alpha = I(.2)) +
-  geom_smooth(size = 2, span = .3) +
-  geom_text(aes(585, -15, label="Coaching change", color = NULL), size = 8) + 
-  scale_fill_viridis(discrete = TRUE) +
-  scale_color_viridis(discrete = TRUE) +
-  guides(fill = guide_legend(title = "Goalie Coach"), color = FALSE) +
+  #geom_vline(xintercept = 530) +
+  geom_point(aes(color = coach, fill = coach), size = 1) +
+  geom_smooth(aes(color = coach, fill = coach), size = 2, span = .3, show.legend = FALSE) +
+  #geom_text(aes(585, -15, label="Coaching change", color = NULL), size = 8) + 
+  #coord_cartesian(ylim = c(-20, 20)) +
+  facet_wrap(~season, ncol = 1) +
+  guides(color = guide_legend(title = "Goalie Coach"), fill = FALSE) +
   labs(y = "Adjusted GSAA per 60", x = "Game Number", title = "Marc-Andre Fleury Goalie Coach Comparison") +
-  theme_nhh()
+  theme_bw()
+
+
+  
 
 maf_summary <- maf %>%
   group_by(coach) %>%
@@ -61,6 +69,15 @@ ggplot(filter(maf_summary, measure == "adjgsaa60_sd"), aes(measure, metric, fill
   guides(fill = guide_legend(title = "Goalie Coach"), color = FALSE) +
   scale_fill_viridis(discrete = TRUE) +
   theme_nhh()
+
+#standard deviation line chart
+maf %>%
+  select(coach, season, adjgsaa60) %>%
+  group_by(season, coach) %>%
+  summarize(stddev = sd(adjgsaa60)) %>%
+  ggplot(., aes(season, stddev, fill = coach, group = 1)) +
+  geom_line() +
+  ylim(c(0, 5))
 
 ggplot(maf, aes(adjgsaa60, fill = coach)) +  
   geom_histogram(binwidth = .5, alpha = I(.8)) +

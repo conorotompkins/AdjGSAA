1be9b658d39014943b681082b87a1f0b0c6f783f
conorotompkins <conorotompkins@gmail.com>
2016-12-03 20:55:47 -0500

    working on graphs

--- graphs/MAF coach/maf coaching graphs.R
+++ graphs/MAF coach/maf coaching graphs.R
@@ -14,6 +14,9 @@ library(viridis)
 source("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/graphs/theme_nhh.R")
 theme_set(theme_nhh())
 
+subtitle <- "Games with >2 TOI"
+caption <- "@NHH_Hockey"
+
 maf <- read.csv(text=getURL("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/combined/adjgsaa.combined.csv")) %>%
   filter(name == "Marc-Andre.Fleury", toi > 2) %>%
   select(game_type, name, season, date, game_number, adjgsaa60) %>%
@@ -32,22 +35,9 @@ ggplot(maf, aes(game_number, adjgsaa60)) +
   geom_smooth(size = 2, span = .3) +
   geom_text(aes(585, -15, label="Coaching change"), size = 8) + 
   guides(color = guide_legend(title = "Goalie Coach"), fill = FALSE) +
-  labs(y = "Adjusted GSAA per 60", x = "Game Number", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = "Games with >2 TOI") +
+  labs(y = "Adjusted GSAA per 60", x = "Game Number", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = caption) +
   theme(plot.caption = element_text(size=12, hjust=1))
 
-ggplot(maf, aes(season_game_number, adjgsaa60)) +
-  geom_hline(yintercept = 0) +
-  #geom_vline(xintercept = 530) +
-  geom_point(aes(color = coach, fill = coach), size = 1) +
-  geom_smooth(aes(color = coach, fill = coach), size = 2, span = .3, show.legend = FALSE) +
-  #geom_text(aes(585, -15, label="Coaching change", color = NULL), size = 8) + 
-  #coord_cartesian(ylim = c(-20, 20)) +
-  facet_wrap(~season, ncol = 1) +
-  guides(color = guide_legend(title = "Goalie Coach"), fill = FALSE) +
-  labs(y = "Adjusted GSAA per 60", x = "Game Number", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = "Games with >2 TOI")
-
-?theme
-  
 
 maf_coach_summary <- maf %>%
   group_by(coach) %>%
@@ -59,7 +49,7 @@ ggplot(maf_coach_summary, aes(coach, adjgsaa60_mean, fill = coach)) +
   coord_cartesian(ylim = c(0, 1)) +
   scale_fill_viridis(discrete = TRUE) +
   scale_y_continuous(expand = c(0, 0)) +
-  labs(y = "Mean AdjGSAA per 60", x = "Goalie Coach", title = "Marc-Andre Fleury Coach Analysis", caption = "Games with >2 TOI") +
+  labs(y = "Mean AdjGSAA per 60", x = "Goalie Coach", title = "Marc-Andre Fleury Coach Analysis", caption = caption) +
   guides(fill = FALSE) +
   theme(plot.caption = element_text(size=12, hjust=1))
 
@@ -68,7 +58,7 @@ ggplot(maf_coach_summary, aes(coach, adjgsaa60_sd, fill = coach)) +
   geom_col() +
   scale_y_continuous(expand = c(0, 0)) +
   coord_cartesian(ylim = c(0, 3)) +
-  labs(y = "Standard Deviation of Adjusted GSAA per 60", x = "Goalie Coach", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = "Games with >2 TOI") +
+  labs(y = "Standard Deviation of Adjusted GSAA per 60", x = "Goalie Coach", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = caption) +
   guides(fill = FALSE) +
   scale_fill_viridis(discrete = TRUE) +
   theme(plot.caption = element_text(size=12, hjust=1))
@@ -81,18 +71,20 @@ maf %>%
   ggplot(., aes(season, stddev, color = coach, group = 1)) +
   geom_line(size = 2) +
   ylim(c(0, 5)) +
-  labs(x = "Season", y = "Standard Deviation of Adjusted GSAA per 60", caption = "Games with >2 TOI") +
+  labs(x = "Season", y = "Standard Deviation of Adjusted GSAA per 60", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = caption) +
+  scale_color_viridis(discrete = TRUE) +
   guides(color = guide_legend(title = "Goalie Coach")) +
-  theme(plot.caption = element_text(size=12, hjust=1))
+  theme(plot.caption = element_text(size=12, hjust=1),
+        plot.subtitle = element_text(size = 12))
 
 ggplot(maf, aes(adjgsaa60, fill = coach)) +  
   geom_histogram(binwidth = .5, alpha = I(.8)) +
   geom_vline(xintercept = 0, size = 1) +
   facet_wrap(~coach, ncol = 1) +
+  coord_cartesian(ylim = c(0, 70)) +
   scale_y_continuous(expand = c(0, 0)) +
   scale_fill_viridis(discrete = TRUE) +
-  scale_color_viridis(discrete = TRUE) +
-  guides(fill = guide_legend(title = "Goalie Coach"), color = FALSE) +
-  labs(x = "Adjusted GSAA per 60", y = "Count of games", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = "Games with >2 TOI") +
+  labs(x = "Adjusted GSAA per 60", y = "Count of games", title = "Marc-Andre Fleury Goalie Coach Comparison", caption = caption) +
   guides(fill = FALSE) +
-  theme(plot.caption = element_text(size=12, hjust=1))
+  theme(plot.caption = element_text(size=12, hjust=1),
+        plot.subtitle = element_text(size = 12))

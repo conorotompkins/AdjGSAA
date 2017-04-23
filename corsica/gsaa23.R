1a6d8a955c179865bf574e1bf4f8ccb7eb4763f6
Conor Tompkins <conorotompkins@gmail.com>
2017-04-23 16:36:44 -0400

    added big goalie CSV, working on adding season colum

--- corsica/gsaa23.R
+++ corsica/gsaa23.R
@@ -3,9 +3,28 @@ library(tidyverse)
 setwd("~/github folder/AdjGSAA/corsica")
 
 theme_set(theme_bw())
-df <- read_csv("1617 5v5 individual game goalie data.csv")
+df <- read_csv("2007-08 to 2016-17 big goalie data.csv")
 
 colnames(df) <- tolower(colnames(df))
+
+#create season colums
+#create date range
+seasons <- data.frame(season = c("2007-2008",
+                                 "2008-2009"),
+                      beginning = as.POSIXct(c("2007-10-01",
+                                               "2008-10-01"), "%y-%m-%d"),
+                      end = as.POSIXct(c("2008-05-01", "2009-05-01"), "%y-%m-%d"))
+
+season20072008 <- data.frame(season = c("2007-2008"),
+                 date = seq.POSIXt(as.POSIXct("2007-10-01"), as.POSIXct("2008-05-01"), by="day"))
+warnings()
+
+first_date <- as.POSIXct(min(my_data_df$trans_date), "%m/%d/%y")
+last_date <- as.POSIXct(max(my_data_df$trans_date), "%m/%d/%y")
+
+ts <- seq.POSIXt(first_date, last_date, by="day")
+ts <- ymd_hms(ts)
+ts <- ymd(substr(ts, 1, 10))
 
 season <- df %>% 
   filter(toi > 20) %>% 
@@ -48,18 +67,41 @@ ggplot(df_xgsaa, aes(gsaa23_avg, gsaa23_sd, label = player)) +
   scale_x_reverse() +
   theme_bw()
 
-predictions <- df %>% 
+predictions <- df %>%
+  group_by(player) %>% 
+  arrange(date) %>% 
+  mutate(game_number = dense_rank(date)) %>% 
+  ungroup() %>% 
+  arrange(player, date) %>% 
   mutate(season = "20162017") %>% 
   left_join(season) %>% 
   filter(sa >= 1, toi >= 5) %>% 
   mutate(diff = ga - xga,
          diffaa = diff - avg_diff,
          gsaa23 = (diffaa / sa) * 23) %>% 
-  select(season, player, date, toi, sa, ga, xga, diff, diffaa, gsaa23) %>% 
-  mutate(prev_gsaa23 = lag(gsaa23),
-         gsaa23_prev_25 = rollmean(x = gsaa23, 25, align = "left", fill = NA),
-         gsaa23_next_25 = rollmean(x = gsaa23, 25, align = "right", fill = NA)) %>% 
+  select(season, player, date, game_number, toi, sa, ga, xga, diff, diffaa, gsaa23) %>% 
+  mutate(gsaa23_prev_25 = rollapply(data = gsaa23, 
+                                    width = 25, 
+                                    FUN = mean, 
+                                    align = "right", 
+                                    fill = NA, 
+                                    na.rm = T),
+         gsaa23_next_25 = rollapply(data = gsaa23, 
+                                    width = 25, 
+                                    FUN = mean, 
+                                    align = "left", 
+                                    fill = NA, 
+                                    na.rm = T)) %>% 
   na.omit()
+
+lundqvist <- predictions %>% 
+  filter(player == "HENRIK.LUNDQVIST") %>% 
+  select(player, game_number, gsaa23, gsaa23_prev_25, gsaa23_next_25) %>% 
+  gather(metric, measure, -c(player, game_number))
+
+
+ggplot(lundqvist, aes(x = game_number, color = metric)) +
+  geom_smooth(aes(y = measure))
 
 ggplot(predictions, aes(gsaa23_next_25, gsaa23_prev_25)) +
   geom_point(alpha = .1) +
